b7c85e9b5d843f9e4e8dbc10d45cb0c62b33d9f6
steeper18k <1253987501@qq.com>
2019-10-23 16:44:25 +0800

    py1

---
+++ py1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+
+list=[]
+i=2
+for i in range (2,100):
+    j=2
+    for j in range(2,i):
+        if(i%j==0):
+            break
+    else:
+        list.append(i)
+print(list) 
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
